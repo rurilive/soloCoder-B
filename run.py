2bb6173860f0d30905bb204ac01d6c8e8e9b5c63
ruri <m5@ruri.live>
2026-04-23 11:37:04 +0800

    888

--- run.py
+++ run.py
@@ -50,6 +50,8 @@ def initdb():
         )
         db.session.add(fib_problem)
         
+        db.session.flush()
+        
         from datetime import date
         DailyProblem.set_for_date(test_problem.id, date.today())
         
@@ -59,4 +61,4 @@ def initdb():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5100)
